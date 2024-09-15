32ac51e526902df356f6b459809a086daf897e9c
Kevinin <kevinin.alves@outlook.com>
2024-09-15 19:22:02 -0300

    aula_12

---
+++ Aula_11/ex009.js
@@ -0,0 +1,11 @@
+//Condição Composta
+
+var pais = 'França'; 
+
+console.log(`Vivendo em ${pais}`);
+
+if (pais == 'Brasil'){
+    console.log('Vocé é Brasileiro!');
+} else {
+    console.log("Você é Estrangeiro!");
+}
